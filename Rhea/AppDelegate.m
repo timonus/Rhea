ba70b94665f23414274ff3a531cd9b0bb98a599c
Tim Johnsen <timonus@gmail.com>
2016-08-03 19:38:37 -0700

    Drag and drop to upload.

--- Rhea/AppDelegate.m
+++ Rhea/AppDelegate.m
@@ -13,13 +13,14 @@
 // Building a status bar app: https://www.raywenderlich.com/98178/os-x-tutorial-menus-popovers-menu-bar-apps
 // Hiding the dock icon: http://stackoverflow.com/questions/620841/how-to-hide-the-dock-icon
 // Handling incoming URLs: http://fredandrandall.com/blog/2011/07/30/how-to-launch-your-macios-app-with-a-custom-url/
+// Drag drop into status bar: http://stackoverflow.com/a/26810727/3943258
 
 static NSString *const kRHEADropboxAppKey = @"";
 static NSString *const kRHEADropboxRedirectURLString = @"";
 
 static NSString *const kRHEADropboxTokenKey = @"dropboxToken";
 
-@interface AppDelegate ()
+@interface AppDelegate () <NSWindowDelegate>
 
 @property (weak) IBOutlet NSWindow *window;
 @property (nonatomic, strong) NSStatusItem *statusItem;
@@ -27,6 +28,8 @@ static NSString *const kRHEADropboxTokenKey = @"dropboxToken";
 @end
 
 @implementation AppDelegate
+
+#pragma mark - App Lifecycle
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -37,6 +40,9 @@ static NSString *const kRHEADropboxTokenKey = @"dropboxToken";
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
     self.statusItem.button.image = [NSImage imageNamed:@"StatusBarButtonImage"];
     self.statusItem.button.action = @selector(statusItemClicked:);
+    
+    [self.statusItem.button.window registerForDraggedTypes:@[NSFilenamesPboardType]];
+    self.statusItem.button.window.delegate = self;
     
     [self updateMenu];
 }
@@ -59,6 +65,8 @@ static NSString *const kRHEADropboxTokenKey = @"dropboxToken";
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
 }
+
+#pragma mark - Menu Management
 
 - (void)statusItemClicked:(id)sender
 {
@@ -90,9 +98,82 @@ static NSString *const kRHEADropboxTokenKey = @"dropboxToken";
     [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://www.dropbox.com/recents"]];
 }
 
+#pragma mark - Drag & Drop
+
+- (NSDragOperation)draggingEntered:(id<NSDraggingInfo>)sender
+{
+    return NSDragOperationCopy;
+}
+
+- (BOOL)performDragOperation:(id<NSDraggingInfo>)sender
+{
+    // http://stackoverflow.com/a/423702/3943258
+    
+    NSArray *const paths = [[sender draggingPasteboard] propertyListForType:NSFilenamesPboardType];
+    
+    if (paths.count == 1) {
+        NSString *const path = [paths firstObject];
+        
+        NSURL *const fileURL = [NSURL fileURLWithPath:path];
+        NSString *const filename = [[fileURL URLByDeletingPathExtension] lastPathComponent];
+        NSString *const extension = [fileURL pathExtension];
+        NSString *const suffix = [self randomSuffix];
+        NSString *const remoteFilename = [NSString stringWithFormat:@"%@-%@%@", filename, suffix, extension.length > 0 ? [NSString stringWithFormat:@".%@", extension] : @""];
+        NSString *const remotePath = [NSString stringWithFormat:@"/%@", remoteFilename];
+        
+        // Begin uploading the file
+        [TJDropbox uploadFileAtPath:path toPath:remotePath accessToken:[self dropboxToken] completion:^(NSDictionary * _Nullable parsedResponse, NSError * _Nullable error) {
+            if (error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    NSAlert *const alert = [[NSAlert alloc] init];
+                    alert.messageText = @"Couldn't upload file";
+                    alert.informativeText = path;
+                    [alert runModal];
+                });
+            }
+        }];
+        
+        // Copy a short link
+        [TJDropbox getShortSharedLinkForFileAtPath:remotePath accessToken:[self dropboxToken] completion:^(NSString * _Nullable urlString) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (urlString) {
+                    [[NSPasteboard generalPasteboard] clearContents];
+                    [[NSPasteboard generalPasteboard] writeObjects:@[urlString]];
+                } else {
+                    NSAlert *const alert = [[NSAlert alloc] init];
+                    alert.messageText = @"Couldn't copy link";
+                    alert.informativeText = path;
+                    [alert runModal];
+                }
+            });
+        }];
+    } else {
+        NSAlert *const alert = [[NSAlert alloc] init];
+        alert.messageText = @"Multiple files aren't supported at this time.";
+        [alert runModal];
+    }
+    
+    return YES;
+}
+
+#pragma mark - Dropbox
+
 - (NSString *)dropboxToken
 {
     return [[NSUserDefaults standardUserDefaults] stringForKey:kRHEADropboxTokenKey];
+}
+
+#pragma mark - Utilities
+
+- (NSString *)randomSuffix
+{
+    static NSString *const kCharacterSet = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    NSMutableString *randomSuffix = [NSMutableString new];
+    for (NSUInteger i = 0; i < 4; i++) {
+        [randomSuffix appendFormat:@"%c", [kCharacterSet characterAtIndex:arc4random_uniform((u_int32_t)kCharacterSet.length)]];
+    }
+    return randomSuffix;
 }
 
 @end
