7c2a4a018b56627e8b4a3479372d57400d540a95
Tim Johnsen <timonus@gmail.com>
2016-08-03 18:49:46 -0700

    Table stakes.

--- Rhea/AppDelegate.m
+++ Rhea/AppDelegate.m
@@ -8,19 +8,45 @@
 
 #import "AppDelegate.h"
 
+// Building a status bar app: https://www.raywenderlich.com/98178/os-x-tutorial-menus-popovers-menu-bar-apps
+// Hiding the dock icon: http://stackoverflow.com/questions/620841/how-to-hide-the-dock-icon
+
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (nonatomic, strong) NSStatusItem *statusItem;
+
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
+    self.statusItem.button.image = [NSImage imageNamed:@"StatusBarButtonImage"];
+    self.statusItem.button.action = @selector(statusItemClicked:);
+    
+    NSMenu *const menu = [[NSMenu alloc] init];
+    [menu addItem:[[NSMenuItem alloc] initWithTitle:@"Recents" action:@selector(recentsMenuItemClicked:) keyEquivalent:@""]];
+    [menu addItem:[NSMenuItem separatorItem]];
+    [menu addItem:[[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"]];
+    self.statusItem.menu = menu;
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (void)applicationWillTerminate:(NSNotification *)aNotification
+{
+}
+
+- (void)statusItemClicked:(id)sender
+{
+    
+}
+
+- (void)recentsMenuItemClicked:(id)sender
+{
+    // http://stackoverflow.com/questions/381021/launch-safari-from-a-mac-application
+    // TODO: Open in new tab.
+    [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:@"https://www.dropbox.com/recents"]];
 }
 
 @end
