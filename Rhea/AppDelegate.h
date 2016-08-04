468dd20b7fe33362b14ebcd39189c3d7fb86a61d
Tim Johnsen <timonus@gmail.com>
2016-08-03 18:28:43 -0700

    Initial Commit

---
+++ Rhea/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rhea
+//
+//  Created by Tim Johnsen on 8/3/16.
+//  Copyright © 2016 tijo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Rhea/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Rhea
+//
+//  Created by Tim Johnsen on 8/3/16.
+//  Copyright © 2016 tijo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
